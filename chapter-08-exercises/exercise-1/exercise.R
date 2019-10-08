c67e23552ab91faa726b5d7e6bffafc85ef1054f
yifu <y.qiu@alumnos.upm.es>
2019-10-08 16:28:45 +0200

    chapter 10

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,36 +1,37 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast = c("bread", "mike")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch = c("rice", "bread")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals = list(b = my_breakfast, l = my_lunch)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
-
+my_dinner = c("pasta", "potato")
+meals = c(meals, d = list(my_dinner))
+print(meals)
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner = meals$d
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
+meals[5] = meals[["l"]]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
-
+early_meals = list(meals["b"],meals["l"])
 
 
 ### Challenge ###
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
-
+number = list(lapply(meals, length))
 
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
--- chapter-10-exercises/exercise-5/exercise.R
+++ chapter-10-exercises/exercise-5/exercise.R
@@ -2,26 +2,36 @@
 
 # Read in the female baby names data file found in the `data` folder into a 
 # variable called `names`. Remember to NOT treat the strings as factors!
-
+names = read.csv("data/female_names.csv", stringsAsFactors = FALSE)
 
 # Create a data frame `names_2013` that contains only the rows for the year 2013
-
+names_2013 = subset(names,names$year == 2013)
 
 # What was the most popular female name in 2013?
+names_2013$name[names_2013$prop == max(names_2013$prop)]
 
 
 # Write a function `most_popular_in_year` that takes in a year as a value and 
 # returns the most popular name in that year
+most_popular_in_year = function(y)
+{
+  names_year = subset(names,names$year == y)
+  res = names_year$name[names_year$prop == max(names_year$prop)]
+  return (res)
+}
+
 
 
 # What was the most popular female name in 1994?
-
+most_popular_in_year(1994)
 
 # Write a function `number_in_million` that takes in a name and a year, and 
 # returns statistically how many babies out of 1 million born that year have 
 # that name. 
 # Hint: get the popularity percentage, and take that percentage out of 1 million.
-
+number_in_million = function(name, year){
+  
+}
 
 # How many babies out of 1 million had the name 'Laura' in 1995?
 
